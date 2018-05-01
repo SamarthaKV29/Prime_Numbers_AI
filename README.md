2199f4b4a60feaff4599f31ca9c0704b10ecc070
SamarthaKV29 <samarthakv29@gmail.com>
2018-05-01 12:55:20 -0400

    2nd

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Prime_Numbers_AI
 Prime number checker/ generator through machine learning.
+## Description
+
+I created a simple machine learning program to generate prime numbers. The system is trained on 100 primes and then it can predict any range of primes.
+
+### Results
--- test1.py
+++ test1.py
@@ -10,22 +10,23 @@ import time
 #
 #fig, ax = plt.subplots()
 #line, = ax.plot([], [], lw=2)
-#ax.grid()
+# ax.grid()
 
-primes = [1,2]
+primes = [1, 2]
 
 
 def primeGen(N):
-    nums = [i for i in range(1,N)]
+    nums = [i for i in range(1, N)]
     for i in nums:
         facs = []
-        for j in range(1,i):
+        for j in range(1, i):
             if(i % j == 0):
                 facs.append(j)
         if(len(facs) == 1):
             primes.append(i)
 
     primes.sort()
+
 
 def genYval(x_arr):
     y_arr = []
@@ -37,27 +38,29 @@ def genYval(x_arr):
     return y_arr
 
 
-
 def Exp1(Xt, Yt, Xv, Yv):
 
-    svm1 = SVC()
-    svm1.fit(Xt, Yt)
-    #Predict
-    pred = svm1.predict(Xv)
+    lrc = LogisticRegression()
+    lrc.fit(Xt, Yt)
+    # Predict
+    pred = lrc.predict(Xv)
     return pred, accuracy_score(Yv, pred)
 
 
 scores = []
 primeGen(100)
 primes = list(set(primes))
-X = [x for x in range(1,100)]
+X = [x for x in range(1, 100)]
 y = genYval(X)
 
-print np.resize(X, [99,1])
-print np.resize(y[99,1])
+print np.size(X)
+print np.size(y)
 
-Xtrain, Xtest, Ytrain, Ytest = model_selection.train_test_split(X,y,test_size=0.2,random_state=0)
+Xtrain, Xtest, Ytrain, Ytest = model_selection.train_test_split(
+    X, y, test_size=0.2, random_state=0)
 
-svm1 = SVC()
-svm1.fit(Xtrain, Ytrain)
-y_pred = svm1.predict(Xtest)
+Xtrain = np.flatten(Xtrain)
+# svm1 = SVC()
+# svm1.fit(Xtrain, Ytrain)
+# y_pred = svm1.predict(Xtest)
+print(Exp1(Xtrain, Ytrain, Xtest, Ytest))
